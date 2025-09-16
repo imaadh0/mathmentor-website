4a1beef54985469527ec78ca47913b80d84a9b1b
AdwelloTech <info@adwello.com>
2025-09-16 16:44:58 +0530

    student

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Student from "./pages/Student";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/student" element={<Student />} />
       </Routes>
     </Router>
   );
